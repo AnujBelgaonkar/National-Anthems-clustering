158c2674a249c7f5dbcefd3d89d94b26d4961a99
AnujBelgaonkar <anuj.belgaonkar@gmail.com>
2024-07-13 16:14:10 +0530

    geodata

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+def main():
+    st.title("National Anthmes Dashboard")
+    left, right = st.columns(2)
+    with left:
+        
+
+    with right:
+
+
+if __name__ == "__main__": 
+    main()
