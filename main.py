bfbe941197bc3c80fe0ff0a7f9b2fe69ccb99c7c
AnujBelgaonkar <anuj.belgaonkar@gmail.com>
2024-07-13 23:43:27 +0530

    map added

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+import folium
+import geopandas as gpd
+import pandas as pd
+from shapely import wkt
 import streamlit as st
-
+from streamlit_folium import st_folium
+from utils.plotmap import load_data,create_map
 st.set_page_config(layout='wide')
-def main():
-    st.title("National Anthmes Dashboard")
-    left, right = st.columns(2)
-    with left:
-        
 
-    with right:
+
+def main():
+    left,right = st.columns(2)
+    with left:
+        clusters = st.slider(label='Cluster Numbers',min_value=5,max_value=20,)
+        gdf = load_data(clusters)
+        m = create_map(gdf)
+        st_folium(m,height=400,width=700)
+
 
 
 if __name__ == "__main__": 
---
+++ utils/plotmap.py
@@ -0,0 +1,30 @@
+import folium
+import geopandas as gpd
+import pandas as pd
+from shapely import wkt
+import streamlit as st
+from streamlit_folium import st_folium
+colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000']
+
+@st.cache_data
+def load_data(clusters):
+    df = pd.read_csv(r"E:\Projects\National Anthems\data\geoplots\kmeans{}.csv".format(clusters))
+    df['geometry'] = df['geometry'].apply(wkt.loads)
+    gdf = gpd.GeoDataFrame(df, geometry='geometry', crs="EPSG:4326")
+    return gdf
+
+def create_map(gdf):
+    m = folium.Map(location=[0, 0], zoom_start=2, max_bounds=True)
+    label_colors = {label: colors[i] for i, label in enumerate(gdf['Label'].unique())}
+    folium.GeoJson(
+        gdf,
+        name='nationalanthems',
+        style_function=lambda feature: {
+            'fillColor': label_colors[feature['properties']['Label']],
+            'color': 'black',
+            'weight': 1,
+            'fillOpacity': 0.5,
+        }
+    ).add_to(m)
+    folium.LayerControl().add_to(m)
+    return m
