005783b8439180f0d436047b32dfaebf4362cd02
AnujBelgaonkar <anuj.belgaonkar@gmail.com>
2024-07-16 09:05:19 +0530

    deployment left

--- main.py
+++ main.py
@@ -30,29 +30,23 @@ def main():
         """,
         unsafe_allow_html=True
     )
-    
-    with st.container():
-        left, right = st.columns(2, gap='medium')
-        with left:
-            k = st.slider(label='Select number of clusters', min_value=5, max_value=20)
-            gdf = load_data(k)
-            if not gdf.empty:
-                with st.container():
-                    m = create_map(gdf)
-                    st_folium(m, height=350, width=650)
-        with right:
-            with st.container(border=False):
-                pieplot(k)
-    
-    st.markdown("<br><br>", unsafe_allow_html=True)  # Adding space between sections
 
-    with st.container():
-        falseleft, middle, falseright = st.columns([1, 2, 1], gap='medium')
-        with middle:
-            st.subheader("Cluster Word Frequency")
+    left, right = st.columns(2, gap='small')
+    with left:
+        k = st.slider(label='Select number of clusters', min_value=5, max_value=20)
+        gdf = load_data(k)
+        if not gdf.empty:
+            with st.container(height=360):  # Wrap the map rendering inside a container
+                m = create_map(gdf)
+                st_folium(m, height=360, width=800)  # Set height and width explicitly
+    with right:
+        with st.container(border=False):
+            pieplot(k)
+    falseleft,middle,falseright = st.columns([1,3,1],gap = 'medium')
+    with middle:
+        with st.container():
             selected_cluster = st.slider(label="Select Cluster", min_value=1, max_value=k)
             freqgraph = plotfreqwords(k, cluster=selected_cluster)
-            st.plotly_chart(freqgraph, use_container_width=True)
-
+            temp = st.plotly_chart(freqgraph, use_container_width=True)
 if __name__ == "__main__": 
     main()
